cd2b8396433b2bec60d0713b93d032cabe8fd875
João Neves <joao95neves@gmail.com>
2020-04-17 14:38:18 +0100

    Change license to GPLv3.0

--- RegexQueryCSharp/RegexQuery.cs
+++ RegexQueryCSharp/RegexQuery.cs
@@ -1,8 +1,8 @@
 /*
  * Copyright (c) 2020 João Pedro Martins Neves (SHIVAYL) - All Rights Reserved.
  *
- * RegexQuery and all its contents are licensed under the GNU Lesser General Public License v3.0
- * (LGPL-3.0), located in the root folder, under the name "LICENSE.md".
+ * RegexQuery and all its contents are licensed under the GNU General Public License v3.0
+ * (GPL-3.0), located in the root folder, under the name "LICENSE.md".
  *
  */
 
