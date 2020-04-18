586e761e5306321e52091dbdc083a210e914213c
João Neves <joao95neves@gmail.com>
2020-04-18 13:44:21 +0100

    Added more Regex tokens and patterns

--- RegexQueryCSharp/RegexQueryPatternExtensions.cs
+++ RegexQueryCSharp/RegexQueryPatternExtensions.cs
@@ -6,10 +6,6 @@
  *
  */
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
 using RegexQueryCSharp.Interfaces;
 using RegexQueryCSharp.Constants;
 using RegexQueryCSharp.Enums;
@@ -18,18 +14,6 @@ namespace RegexQueryCSharp
 {
     public static class RegexQueryPatternExtensions
     {
-        public static IRegexQuery ASpace(this IRegexQuery regexQueryBuilder)
-        {
-            regexQueryBuilder.Query.Append( RegexExpressions.Space );
-            return regexQueryBuilder;
-        }
-
-        public static IRegexQuery ADigit(this IRegexQuery regexQueryBuilder)
-        {
-            regexQueryBuilder.Query.Append( RegexExpressions.Digit );
-            return regexQueryBuilder;
-        }
-
         public static IRegexQuery ADate(this IRegexQuery regexQueryBuilder)
         {
             return RegexQueryPatternExtensions.ADate( regexQueryBuilder, Separator.All );
@@ -37,16 +21,16 @@ namespace RegexQueryCSharp
 
         public static IRegexQuery ADate(this IRegexQuery regexQueryBuilder, Separator separator = Separator.All)
         {
-            // [0-3]?[0-9]
-            regexQueryBuilder.Query.Append( RegexExpressions.ValueBetween( '0', '3' ) ).Append( '?' ).Append( RegexExpressions.ValueBetween( '0', '9' ) )
+                                   // [0-3]?[0-9]
+            regexQueryBuilder.Query.Append( RegexTokens.ValueBetween( '0', '3' ) ).Append( '?' ).Append( RegexTokens.ValueBetween( '0', '9' ) )
                                    // (\/|\.|-)
                                    .Append( "(" ).Append( Separators.Resolve( separator ) ).Append( ")" )
                                    // [0-3]?[0-9]
-                                   .Append( RegexExpressions.ValueBetween( '0', '3' ) ).Append( '?' ).Append( RegexExpressions.ValueBetween( '0', '9' ) )
+                                   .Append( RegexTokens.ValueBetween( '0', '3' ) ).Append( '?' ).Append( RegexTokens.ValueBetween( '0', '9' ) )
                                    // (\/|\.|-)
                                    .Append( "(" ).Append( Separators.Resolve( separator ) ).Append( ")" )
                                    // [1-9]\d{3}
-                                   .Append( RegexExpressions.ValueBetween( '1', '9' ) ).Append( RegexExpressions.Digit ).Append( RegexExpressions.QuantityOfPreceding( 3 ) );
+                                   .Append( RegexTokens.ValueBetween( '1', '9' ) ).Append( RegexTokens.Digit ).Append( RegexTokens.QuantityOfPreceding( 3 ) );
 
             return regexQueryBuilder;
         }
